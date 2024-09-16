bc0eb3ebb77ead2d10c3dd372ea16dc251a50510
emiran0 <emirhankrtll@gmail.com>
2024-09-16 15:41:32 +0300

    Initial Commit

---
+++ WordleApp/WordleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordleAppApp.swift
+//  WordleApp
+//
+//  Created by Emiran Kartal on 16.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct WordleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
